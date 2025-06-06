eff0947cffa70f5cd15b2e0a8b4ad47434357905
Guilherme De Noni Marchioro <54208652+marchiorog@users.noreply.github.com>
2025-06-06 15:59:33 -0300

    Espaçamento

--- src/screens/MeuPerfil.tsx
+++ src/screens/MeuPerfil.tsx
@@ -17,6 +17,7 @@ const { width, height } = Dimensions.get('window');
 type MeuPerfilScreenNavigationProp = StackNavigationProp<RootStackParamList, 'MeuPerfil'>;
 
 type Props = {
+  
   navigation: MeuPerfilScreenNavigationProp;
 };
 
