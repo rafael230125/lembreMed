f19794177ce8dd244c552593c769d48133c11c87
Guilherme De Noni Marchioro <guimarchioro@hotmail.com>
2025-10-14 13:09:36 -0300

    Correções globais. 1#

--- env.d.ts
+++ env.d.ts
@@ -5,6 +5,5 @@ declare module '@env' {
   export const FIREBASE_STORAGE_BUCKET: string;
   export const FIREBASE_MESSAGING_SENDER_ID: string;
   export const FIREBASE_APP_ID: string;
-  export const GOOGLE_VISION_API_KEY: string;
   export const GEMINI_API_KEY: string;
 }
